d6493cbcdfe8f0eef6a77cf7d0344e7d3829d3cf
Akhmed <akhmedturan@gmail.com>
2023-09-12 13:54:17 +0600

    up date-N1 js

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 Пройдитесь циклом и выведите только те числа, которые начинаются на 2 и 5. 
 Результат должен вывестись в новом массиве newArray */
 var arrayNumber = [123, 234, 256, 345, 456, 567, 543, 67, 55, 23, 28, 50];
-var newArray = [];
-for(var i = 0; i < arrayNumber.length; i++){
-    var arrayNumberStr = arrayNumber[i].toString()
-    if (arrayNumberStr.startsWith('2') || arrayNumberStr.startsWith('5')) {
-        newArray.push(arrayNumber[i])
+var newArray = []
+for(var i of arrayNumber){
+    var arrayNumberStr = i.toString()
+    if(arrayNumberStr.startsWith('2') || arrayNumberStr.startsWith('5')){
+        newArray.push(i)
     }
 }
 console.log(newArray);
-
+// -------------------------
 /* ⁃ Написать функцию (любой из видов пройденных на уроке), 
 которая принимает аргумент и возвращает его тип данных в консоли.
 Пример:    
@@ -21,7 +21,7 @@ function argumentsTypeOf(params) {
     console.log(typeof params);
 }
 argumentsTypeOf();
-
+// -------------------------
 /* ⁃ Дан список тегов  ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1',  'p', 'h1', 'ol’, ‘br’]. 
 Вам нужно посчитать каких тегов сколько штук.  Обязательные условия:  
 1. Конечный результат должен вывестись как объект {}, где ключ - это сам тег, 
